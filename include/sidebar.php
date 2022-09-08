6080933e68137d966d6c02d1b8d88c6e437ad209
Tobija Žuntar <tobija.zuntar@gmail.com>
2022-09-08 09:24:30 +0200

    Finish the top navigation bar CSS, split the navbar and sidebar into their own files, reorganize styles.css

---
+++ include/sidebar.php
@@ -0,0 +1,24 @@
+<div class="sidebar">
+    <a href="index.php" class="logo">
+        <img src="./assets/img/logo.svg" alt="Logo"/>
+        EpicTunes
+    </a>
+    <nav>
+        <p>Menu</p>
+        <ul>
+            <li><a href="index.php"><img src="./assets/img/icons/home.svg" alt=""/>My Music</a></li>
+            <li><img src="./assets/img/icons/explore.svg" alt=""/>Discover</li>
+            <li><img src="./assets/img/icons/search.svg" alt=""/>Search</li>
+        </ul>
+        <p>Library</p>
+        <ul>
+            <li><img src="./assets/img/icons/album.svg" alt=""/>Albums</li>
+            <li><img src="./assets/img/icons/artist.svg" alt=""/>Artists</li>
+        </ul>
+        <p>Others</p>
+        <ul>
+            <li><img src="./assets/img/icons/settings.svg" alt=""/>Settings</li>
+            <li><a href="logout.php"><img src="./assets/img/icons/logout.svg" alt=""/>Log Out</a></li>
+        </ul>
+    </nav>
+</div>
---
+++ include/top-nav.php
@@ -0,0 +1,13 @@
+<nav>
+    <div class="search-box">
+        <label>
+            <input type="text" placeholder="Search..." name="search-box"/>
+        </label>
+    </div>
+    <div class="profile-photo">
+        <div class="flex-container">
+            <img src="./assets/img/icons/avatar.svg" alt=""/>
+            <p>John Doe</p>
+        </div>
+    </div>
+</nav>
