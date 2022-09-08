894593e3b3f0c67d09d27928cf761f310b172289
Tobija Žuntar <tobija.zuntar@gmail.com>
2022-09-08 09:24:30 +0200

    Finish the top navigation bar CSS, split the navbar and sidebar into their own files, reorganize styles.css

--- index.php
+++ index.php
@@ -14,40 +14,9 @@ require_once 'include/database.php';
     <link rel="stylesheet" href="./assets/styles.css"/>
 </head>
 <body>
-<div class="sidebar">
-    <a href="index.php" class="logo">
-        <img src="./assets/img/logo.svg" alt="Logo"/>
-        EpicTunes
-    </a>
-    <nav>
-        <p>Menu</p>
-        <ul>
-            <li><a href="index.php"><img src="./assets/img/icons/home.svg" alt=""/>My Music</a></li>
-            <li><img src="./assets/img/icons/explore.svg" alt=""/>Discover</li>
-            <li><img src="./assets/img/icons/search.svg" alt=""/>Search</li>
-        </ul>
-        <p>Library</p>
-        <ul>
-            <li><img src="./assets/img/icons/album.svg" alt=""/>Albums</li>
-            <li><img src="./assets/img/icons/artist.svg" alt=""/>Artists</li>
-        </ul>
-        <p>Others</p>
-        <ul>
-            <li><img src="./assets/img/icons/settings.svg" alt=""/>Settings</li>
-            <li><a href="logout.php"><img src="./assets/img/icons/logout.svg" alt=""/>Log Out</a></li>
-        </ul>
-    </nav>
-</div>
+***REMOVED*** include_once 'include/sidebar.php' ?>
 <div class="root-container">
-    <div class="top">
-        <label>
-            <input type="text" placeholder="Search..." name="search-box"/>
-        </label>
-        <div class="profile-photo">
-            <img src="./assets/img/icons/avatar.svg" alt=""/>
-            John Doe
-        </div>
-    </div>
+    ***REMOVED*** include_once 'include/top-nav.php' ?>
 
     <main>
         <h1>My Music</h1>
