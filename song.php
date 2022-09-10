ba96e6f9ab34cdc1d35cbc2f9c25125cb676974e
Tobija Žuntar <tobija.zuntar@gmail.com>
2022-09-10 09:48:58 +0200

    [WIP] Add code for MP3 playback on the song.php page, redesign the playback container to fit into the rest of the UI

--- song.php
+++ song.php
@@ -19,15 +19,35 @@ include_once 'include/header.php';
 include_once 'include/sidebar.php' ?>
     <div class="root-container">
         <?php include_once 'include/top-nav.php' ?>
+        <link rel="stylesheet" href="./assets/mp3player.css"/>
 
         <main>
             <div class="margin-top-20">
-                <section class="song-player-division flex-container">
-                    <div class="flex-child-half">
-                        <button id="playButton" class="play-button">&gt;</button>
-                        <?= $songData['name'] ?>
+                <section class="song-player-division grid-container">
+                    <div class="grid-col">
+                        <button id="playButton" class="play-button">
+                            <img id="playIcon" src="./assets/img/icons/play.svg" alt="Play"/>
+                        </button>
                     </div>
-                    <div class="flex-child-half">
+                    <div class="grid-col">
+                        <h1 class="song-name"><?= $songData['name'] ?></h1>
+                        <div id="mp3player-container">
+                            <div id="mp3player">
+                                <canvas id="analyzer-render"></canvas>
+                            </div>
+                            <label>
+                                <input id="seek-slider" type="range" min="0" max="500" value="0" step="1">
+                            </label>
+                            <div id="time-box">
+                                <span id="current-time">00:00</span>| <span id="duration">00:00</span>
+                            </div>
+                            <button id="mute-button"></button>
+                            <label>
+                                <input id="volume-slider" type="range" min="0" max="100" value="100" step="1">
+                            </label>
+                        </div>
+                    </div>
+                    <div class="grid-col">
                         <img src="<?= $albumArt ?>" class="album-art-big"
                              id="albumArtBox" alt="Album Art"/>
                     </div>
@@ -47,10 +67,12 @@ include_once 'include/sidebar.php' ?>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/vibrant.js/1.0.0/Vibrant.min.js"
             integrity="sha512-V6rhYmJy8NZQF8F0bhJiTM0iI6wX/FKJoWvYrCM15UIeb6p38HjvTZWfO0IxJnMZrHWUJZJqLuWK0zslra2FVw=="
             crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="./utils/mp3player.js"></script>
     <script>
         $(document).ready(function () {
-            applyBgColorCss($('.song-player-division')[0],
-                getImgSignificantColorRgb('albumArtBox'))
+            // applyBgColorCss($('.song-player-division')[0],
+            // getImgMutedColorRgb('albumArtBox'));
+            $('.song-name')[0].style.color = getImgVibrantColorRgb('albumArtBox');
         });
     </script>
 <?php include_once 'include/footer.php' ?>
