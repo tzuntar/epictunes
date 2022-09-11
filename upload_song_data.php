bc4d8dfccefe3672a1ca4f15a1312123556a102d
Tobija Žuntar <tobija.zuntar@gmail.com>
2022-09-11 10:56:12 +0200

    Basic structure of the upload pages

---
+++ upload_song_data.php
@@ -0,0 +1,27 @@
+***REMOVED***
+***REMOVED***
+if (!isset($_SESSION['identifier']))
+    header('Location: login.php');
+if ($_FILES['mp3file']['name'] == 0)
+    header('Location: upload_song.php');
+
+$mp3file = 'userdata/music/' . $_FILES['mp3file']['name'];
+move_uploaded_file($_FILES['mp3file']['tmp_name'], $mp3file);
+
+include_once 'include/header.php';
+include_once 'include/sidebar.php' ?>
+<div class="root-container">
+    ***REMOVED*** include_once 'include/top-nav.php' ?>
+
+    <main class="padding-20">
+        <h1 class="step-number">2</h1>
+        <h2 class="step-heading accent">Edit Data</h2>
+        <form class="margin-top-20" action="upload_song_confirm.php" method="post"
+              enctype="multipart/form-data">
+            <div class="grid-container">
+
+            </div>
+        </form>
+    </main>
+</div>
+***REMOVED*** include_once 'include/footer.php' ?>
