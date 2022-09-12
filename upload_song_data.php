f22bf327baa1cb4f3e7d38aec2e48c52402f0b69
Tobija Žuntar <tobija.zuntar@gmail.com>
2022-09-12 16:28:27 +0200

    An attempt to improve layout of the upload_song_data.php page

--- upload_song_data.php
+++ upload_song_data.php
@@ -18,8 +18,40 @@ include_once 'include/sidebar.php' ?>
         <h2 class="step-heading accent">Edit Data</h2>
         <form class="margin-top-20" action="upload_song_confirm.php" method="post"
               enctype="multipart/form-data">
-            <div class="grid-container">
-
+            <div class="grid-container meta-edit-grid">
+                <!-- ToDo: find out why the grid placement doesn't work correctly here -->
+                <div class="grid-col">
+                    <div class="grid-row">
+                        <p>Title:</p>
+                    </div>
+                    <div class="grid-row">
+                        <p>Artists:</p>
+                    </div>
+                    <div class="grid-row">
+                        <p>Album:</p>
+                    </div>
+                    <div class="grid-row">
+                        <p>...by:</p>
+                    </div>
+                    <div class="grid-row">
+                        <p>Genre:</p>
+                    </div>
+                    <div class="grid-row">
+                        <p>Tags:</p>
+                    </div>
+                </div>
+                <div class="grid-col">
+                    <input type="text" name="title" required placeholder="Song title"/>
+                    <input type="text" name="artist" required placeholder="Artist names (separated by commas)"/>
+                    <input type="text" name="album" required placeholder="Album name"/>
+                    <input type="text" name="album_artist" required
+                           placeholder="Album artist names (separated by commas)"/>
+                    <input type="text" name="genre" required placeholder="Song genre"/>
+                    <input type="text" name="tags" placeholder="Song tags (separated by commas)"/>
+                </div>
+                <div class="grid-col">
+                    <img src="" alt="Album Art" class="album-art-big"/>
+                </div>
             </div>
         </form>
     </main>
