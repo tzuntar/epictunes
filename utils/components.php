a16550292d7f20e5569d46ab4da1ef58cb93c588
Tobija Žuntar <tobija.zuntar@gmail.com>
2022-09-16 19:46:56 +0200

    Add the album.php page with info and list of songs for a particular album

--- utils/components.php
+++ utils/components.php
@@ -51,11 +51,11 @@ function render_album_list($albums) {
                          alt="Album Art"/>
                 </div>
                 <div>
-                    <p><?= $album->name ?></p>
+                    <p><a href="album.php?id=<?= $album->id ?>"><?= $album->name ?></a></p>
                     <p class="sub-label">
                         <?php foreach ($album->artists as $artist) { ?>
-                            <p><a href="artist.php?id=<?= $artist->id ?>"><?= $artist->name ?></a></p>
-                        <?php } ?>
+                    <p><a href="artist.php?id=<?= $artist->id ?>"><?= $artist->name ?></a></p>
+                    <?php } ?>
                     </p>
                 </div>
             </div>
