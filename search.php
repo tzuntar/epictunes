1186e66d525fe31f9206c3060f9c59a36f963207
Tobija Žuntar <tobija.zuntar@gmail.com>
2022-09-16 19:08:21 +0200

    Improve the search UX, fix dead links in song.php

--- search.php
+++ search.php
@@ -16,14 +16,21 @@ include_once 'include/sidebar.php' ?>
 
     <main>
         <div class="margin-top-20">
+            ***REMOVED*** if (isset($searchResultSongs)) { ?>
             <h2 class="accent padding-20">Search Results</h2>
             <div class="margin-top-20">
-                ***REMOVED*** if (isset($searchResultSongs) && $searchResultSongs !== false) {
+                ***REMOVED*** if ($searchResultSongs !== false && sizeof($searchResultSongs) > 0) {
                     render_song_list($searchResultSongs);
             ***REMOVED*** else { ?>
                     <div class="full-center text-center">
                         <h1 class="full-alert">No Results</h1>
                         <p class="fine-print">Try rewording your query</p>
+                    </div>
+                ***REMOVED*** }
+            ***REMOVED*** else { ?>
+                    <div class="full-center text-center">
+                        <h1 class="full-alert">Type in the box above to search</h1>
+                        <p class="fine-print">Type in the search box on the top of the page to search for songs</p>
                     </div>
                 ***REMOVED*** } ?>
             </div>
