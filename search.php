590c60bf77506d121b1ec4cb1aa55e139c305c69
Tobija Žuntar <tobija.zuntar@gmail.com>
2022-09-17 07:45:00 +0200

    Improve the search function to enable searching for albums and artists in addition to songs

--- search.php
+++ search.php
@@ -5,8 +5,13 @@ if (!isset($_SESSION['identifier']))
 require_once 'utils/queries.php';
 require_once 'utils/components.php';
 
-if (isset($_GET['query'])) {
+if (isset($_GET['query']) && trim($_GET['query']) !== '') {
     $searchResultSongs = Song::get_by_searching($_GET['query']);
+    $songsFound = $searchResultSongs !== false && sizeof($searchResultSongs) > 0;
+    $searchResultAlbums = Album::get_by_searching($_GET['query']);
+    $albumsFound = $searchResultAlbums !== false && sizeof($searchResultAlbums) > 0;
+    $searchResultArtists = Artist::get_by_searching($_GET['query']);
+    $artistsFound = $searchResultArtists !== false && sizeof($searchResultArtists) > 0;
 }
 
 include_once 'include/header.php';
@@ -16,11 +21,22 @@ include_once 'include/sidebar.php' ?>
 
     <main>
         <div class="margin-top-20">
-            ***REMOVED*** if (isset($searchResultSongs)) { ?>
+            ***REMOVED*** if (isset($_GET['query']) && trim($_GET['query']) !== '') { ?>
             <h2 class="accent padding-20">Search Results</h2>
             <div class="margin-top-20">
-                ***REMOVED*** if ($searchResultSongs !== false && sizeof($searchResultSongs) > 0) {
-                    render_song_list($searchResultSongs);
+                ***REMOVED*** if ($songsFound || $albumsFound || $artistsFound) {
+                    if ($songsFound) {
+                        echo '<h2 class="accent margin-lr-20 light-underline">Songs</h2>';
+                        render_song_list($searchResultSongs);
+                ***REMOVED***
+                    if ($albumsFound) {
+                        echo '<h2 class="accent margin-lr-20 light-underline">Albums</h2>';
+                        render_album_list($searchResultAlbums);
+                ***REMOVED***
+                    if ($artistsFound) {
+                        echo '<h2 class="accent margin-lr-20 light-underline">Artists</h2>';
+                        render_artist_list($searchResultArtists);
+                ***REMOVED***
             ***REMOVED*** else { ?>
                     <div class="full-center text-center">
                         <h1 class="full-alert">No Results</h1>
@@ -30,7 +46,8 @@ include_once 'include/sidebar.php' ?>
             ***REMOVED*** else { ?>
                     <div class="full-center text-center">
                         <h1 class="full-alert">Type in the box above to search</h1>
-                        <p class="fine-print">Type in the search box on the top of the page to search for songs</p>
+                        <p class="fine-print">Type in the search box at the top of the page to search for songs, albums,
+                            and artists</p>
                     </div>
                 ***REMOVED*** } ?>
             </div>
