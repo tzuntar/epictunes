6a04f4faa801c92777af73f371dfb0161c74620f
Tobija Žuntar <tobija.zuntar@gmail.com>
2022-09-27 09:31:33 +0200

    Add upload stats to the admin_stats.php page

--- admin_stats.php
+++ admin_stats.php
@@ -16,7 +16,54 @@ include_once 'include/sidebar.php' ?>
         <main>
             <h2 class="accent padding-20"><?= $document_title ?></h2>
             <div class="margin-top-20">
-
+                <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.9.1/chart.min.js"
+                        integrity="sha512-ElRFoEQdI5Ht6kZvyzXhYG9NqjtkmlkfYk0wr6wHxU9JEHakS7UJZNeml5ALk+8IKlU6jDgMabC3vkumRokgJA=="
+                        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+                <canvas id="uploadStats" width="400" height="400"></canvas>
+                <script>
+                    const monthNames = ["January", "February", "March", "April", "May", "June",
+                        "July", "August", "September", "October", "November", "December"];
+                    const data = JSON.parse('***REMOVED***
+                        $stats = get_upload_stats_per_month(6);
+                        for ($minusMonth = 0; $minusMonth < 6; $minusMonth++) {
+                            $month = date('m', strtotime("-$minusMonth month"));
+                            foreach ($stats as $s)  // fill the fields of all 6 months even if there's no data for them
+                                $results[$month] = $s[0] == $month ? $s[1] : 0;
+                    ***REMOVED***
+                        if (isset($results))    // [month][count]... -> [count]...
+                            echo json_encode($results);
+                        ?>');
+                    let months = [];    // map month numbers to names
+                    Object.keys(data).reverse().forEach(monthNum => {
+                        months.push(monthNames[parseInt(monthNum)])
+                ***REMOVED***);
+                    const ctx = document.getElementById('uploadStats').getContext('2d');
+                    const uploadStatsChart = new Chart(ctx, {
+                        type: 'line',
+                        data: {
+                            labels: months,
+                            datasets: [{
+                                label: '# of Uploaded Songs per Month in the Last 6 Months',
+                                data: Object.values(data).reverse(),
+                                backgroundColor: ['#4343EF'],
+                                borderColor: ['#4343EF'],
+                                borderWidth: 2
+                        ***REMOVED***]
+                    ***REMOVED***,
+                        options: {
+                            responsive: true,
+                            title: {
+                                display: true,
+                                text: 'Uploads'
+                        ***REMOVED***,
+                            scales: {
+                                y: {
+                                    beginAtZero: true
+                            ***REMOVED***
+                        ***REMOVED***
+                    ***REMOVED***
+                ***REMOVED***);
+                </script>
             </div>
         </main>
 
