e4d5c3a68f78daa68d37fa8ab270405097a19742
Tobija Žuntar <tobija.zuntar@gmail.com>
2022-10-06 20:10:00 +0200

    Delete the mp3 file when deleting a song

--- delete_song.php
+++ delete_song.php
@@ -6,6 +6,9 @@ if (!isset($_GET['id']))
     header('Location: ' . $_SERVER['HTTP_REFERER']);
 require_once 'utils/queries.php';
 $song = Song::get($_GET['id']);
-if ($_SESSION['is_admin'] || $song->check_ownership($_SESSION['id']))
+$filename = $mp3path = 'userdata/music/' . $song->file_url;;
+if ($_SESSION['is_admin'] || $song->check_ownership($_SESSION['id'])) {
     $song->delete();
+    unlink($filename);
+}
 header('Location: ' . $_SERVER['HTTP_REFERER']);
